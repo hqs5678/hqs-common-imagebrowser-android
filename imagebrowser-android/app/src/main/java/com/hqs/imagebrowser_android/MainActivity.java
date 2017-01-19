72604649aba9fb65e195311993e832984e84e0da
黄青松 <huangqingsong_5678@hotmail.com>
2017-01-19 16:11:02 +0800

    update test

--- imagebrowser-android/app/src/main/java/com/hqs/imagebrowser_android/MainActivity.java
+++ imagebrowser-android/app/src/main/java/com/hqs/imagebrowser_android/MainActivity.java
@@ -24,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
         ImageBrowser.placeHolderImageRes = R.mipmap.ic_launcher;
         ArrayList<String> filePaths = new ArrayList<>();
         filePaths.add("http://d.hiphotos.baidu.com/image/pic/item/10dfa9ec8a136327a1de913a938fa0ec08fac78c.jpg");
+        filePaths.add("http://www.7x24home.com/upload/pics/beijing/2017/01/19/WX20160520991978SM991986/2017-01-19-144216193094.png");
 
         filePaths.add(SDCardUtils.getSDCardPath() + "000.jpg");
         ImageBrowser.show(MainActivity.this, filePaths, 0);
